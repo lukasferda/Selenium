ed37b2249af155bc23927a61afaac8c77ac26ac1
Lukas Ferda <LFerda@moravia.com>
2020-09-07 16:42:10 +0200

    add correct url and more solutions

--- src/Main.java
+++ src/Main.java
@@ -19,18 +19,35 @@ public class Main {
 
         WebDriver driver = new FirefoxDriver(opts);
 
+        driver.manage().window().maximize();
+
         sleep(2000);
 
-        driver.navigate().to("http://aws-demo.shopizer.com/shop/");
+        var baseUrl = "https://czechitas-shopizer.azurewebsites.net/shop/";
+
+        driver.navigate().to(baseUrl);
+        //https://czchitasshopizer.azurewebsites.net/admin/
+        //username:admin@shopizer.com password:password
         sleep(2000);
 
         var a = driver.findElement(By.id("searchField"));
         a.sendKeys("aaa");
         var b = a.getText();
 
+        sleep(2000);
 
         File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         System.out.println(srcFile.getAbsolutePath());
+
+        var title = driver.getTitle();
+        System.out.println(title + " " + title.length());
+
+        var url = driver.getCurrentUrl();
+        System.out.println(url);
+
+        System.out.println(url.equals(baseUrl));
+
+        System.out.println(driver.getPageSource().length());
 
         driver.quit();
     }
